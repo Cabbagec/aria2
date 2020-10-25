6c4b9d449a6fcc8799e32c5ebc991d77b7c8332c
root <root@everdream.xyz>
2020-10-25 15:30:55 +0000

    fix release script

--- docker-release.sh
+++ docker-release.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 id=$(docker create aria2-mingw)
 tag=$(git describe --tags --abbrev=0)
-docker cp $id:/aria2/src/aria2c.exe ./aria2c.exe
-docker rm -v $id
+docker cp $id:/aria2/src/aria2c.exe ./aria2c.exe > /dev/null 2>&1
+docker rm -v $id > /dev/null 2>&1
 
 git log -n 10 > ./ChangeLog
 
-zip -9 "aria2-${tag}-win64".zip NEWS AUTHORS README.mingw LICENSE.OpenSSL ChangeLog COPYING README.html aria2c.exe
+zip -9 "aria2-${tag}-win64".zip NEWS AUTHORS README.mingw LICENSE.OpenSSL ChangeLog COPYING README.rst aria2c.exe > /dev/null 2>&1
 
 echo "aria2-${tag}-win64.zip"
 
